f9336782ae15d6ef51d0fc9c0f681c416f287699
Juan <juaoose@users.noreply.github.com>
2022-01-21 02:36:34 +0000

    generate point features

--- index.js
+++ index.js
@@ -8,24 +8,31 @@ var xhr = require('./lib/xhr.js'),
 
 function loadRuns(STRAVA_TOKEN) {
     return (new Source({ STRAVA_TOKEN: STRAVA_TOKEN }))
-        .pipe(through2(function(chunk, enc, callback) {
+        .pipe(through2(function (chunk, enc, callback) {
             this.emit('debug', 'pushing ' + chunk.length + ' runs into stream');
-            chunk.forEach(function(c) {
+            chunk.forEach(function (c) {
                 this.push(c.id);
             }.bind(this));
             callback();
         }))
-        .pipe(through2(function(id, enc, callback) {
-            xhr.getActivity(STRAVA_TOKEN, id, function(err, res) {
-               if (err) console.error(err);
-               // purposely avoid throwing on errors
-               callback(null, res);
-           });
+        .pipe(through2(function (id, enc, callback) {
+            xhr.getActivity(STRAVA_TOKEN, id, function (err, res) {
+                if (err) {
+                    console.error(err);
+                }
+                // purposely avoid throwing on errors
+                callback(null, res);
+            });
         }))
         .pipe(filter(Array.isArray))
-        .pipe(through2(function(chunk, enc, callback) {
-            var converted = convert(chunk);
-            callback(null, converted);
+        .pipe(through2(function (chunk, enc, callback) {
+            const converted = convert(chunk);
+            if(converted) {
+                callback(null, ...converted);
+            }
+            else {
+                callback(null, converted);
+            }
         }))
         .pipe(geojsonStream.stringify());
 }
